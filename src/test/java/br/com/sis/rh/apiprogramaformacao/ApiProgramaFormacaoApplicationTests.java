276020d9ebedc3fd334a4e757720b0c6362d146d
kaiquilopessis <kaiquilopes@sisconsultoria.com.br>
2021-09-10 16:11:36 -0300

    adicionando projeto

---
+++ src/test/java/br/com/sis/rh/apiprogramaformacao/ApiProgramaFormacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sis.rh.apiprogramaformacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProgramaFormacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
