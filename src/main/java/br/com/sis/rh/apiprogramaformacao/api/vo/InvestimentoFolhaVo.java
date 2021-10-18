c4d8be91e460ee74f9635ee3f00619abdbaaeb33
Marco Aguiar <marcoaguiar@sisconsultoria.com.br>
2021-10-18 09:55:29 -0300

    Aplicação dos VOs

---
+++ src/main/java/br/com/sis/rh/apiprogramaformacao/api/vo/InvestimentoFolhaVo.java
@@ -0,0 +1,28 @@
+package br.com.sis.rh.apiprogramaformacao.api.vo;
+
+public class InvestimentoFolhaVo {
+    private Long id;
+    private String nome;
+    private String status;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
---
+++ src/main/java/br/com/sis/rh/apiprogramaformacao/api/vo/InvestimentoInstrutorVo.java
@@ -0,0 +1,36 @@
+package br.com.sis.rh.apiprogramaformacao.api.vo;
+
+public class InvestimentoInstrutorVo {
+    private Long id;
+    private String nome;
+    private String programa;
+    private String status;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getPrograma() {
+        return programa;
+    }
+    public void setPrograma(String programa) {
+        this.programa = programa;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
