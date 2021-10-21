439aa5087ab9b21ae811213deb7ad41fbe2033f1
Guilherme Melo <guilhermeappache@hotmail.com>
2021-10-21 13:39:04 -0300

    Apagando
    ...

--- src/main/java/br/com/sis/rh/apiprogramaformacao/core/enums/ResultadoConclusao.java
+++
@@ -1,9 +0,0 @@
-package br.com.sis.rh.apiprogramaformacao.core.enums;
-
-public enum ResultadoConclusao {
-	REAJUSTE_SALARIO,
-	NAO_REAJUSTE_SALARIO,
-	
-	EFETIVADO,
-	NAO_EFETIVADO;
-}
--- src/main/java/br/com/sis/rh/apiprogramaformacao/core/enums/StatusConclusao.java
+++
@@ -1,6 +0,0 @@
-package br.com.sis.rh.apiprogramaformacao.core.enums;
-
-public enum StatusConclusao {
-	PROGRESSIVA,
-	FINAL;
-}
