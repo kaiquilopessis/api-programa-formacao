01c676b2d274671575e4faed07dcebdf98166d27
Bruno Agiani <brunoaalisboa@gmail.com>
2021-10-26 13:43:08 -0300

    Criação da Classe Programa

---
+++ src/main/java/br/com/sis/rh/apiprogramaformacao/core/repository/ProgramaRepository.java
@@ -0,0 +1,11 @@
+package br.com.sis.rh.apiprogramaformacao.core.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.sis.rh.apiprogramaformacao.api.model.Programa;
+
+@Repository
+public interface ProgramaRepository extends JpaRepository<Programa, String> {
+   
+}
