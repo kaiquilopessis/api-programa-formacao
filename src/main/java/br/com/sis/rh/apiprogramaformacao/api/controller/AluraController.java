880c9782149f403db34422024e8ec3fedebac2ae
felipesalmazo82 <felipesalmazo@sisconsultoria.com.br>
2021-11-17 15:52:51 -0300

    Comentários

--- src/main/java/br/com/sis/rh/apiprogramaformacao/api/controller/AluraController.java
+++ src/main/java/br/com/sis/rh/apiprogramaformacao/api/controller/AluraController.java
@@ -21,22 +21,27 @@ import br.com.sis.rh.apiprogramaformacao.core.service.AluraService;
 @RequestMapping("/api/alura")
 public class AluraController {
 
-	// precisamos passar para o service os ifs de tratamento
+	//Controller direcionado para listagem/cadastro/exclusão dos registros relacionados à Alura. As lógicas estão
+	//nas classes Service encontradas no pacote br.com.sis.rh.apiprogramaformacao.core.service.
 
 	@Autowired
 	private AluraService aluraService;
-
+	
+	//Listagem dos registros(Retornando uma lista com campos para a exibição), recebe o CPF do participante na requisição para
+	//retornar os registros de um participante em específico.
 	@GetMapping("/{cpf}")
 	public List<AluraDto> listaRegistros(@PathVariable String cpf) {
 		return aluraService.listaRegistros(cpf);
 	}
-
+	
+	//Cadastro dos registros. Recebe o CPF do participante na requisição para realizar o cadastro específico.
 	@PostMapping("/novo/{cpf}")
 	public ResponseEntity<AluraDto> cadastrar(@PathVariable String cpf, @RequestBody AluraForm aluraForm,
 			UriComponentsBuilder uriComponentsBuilder) {
 		return aluraService.cadastrar(cpf, aluraForm, uriComponentsBuilder);
 	}
-
+	
+	//Delete dos registros. Recebe o ID do registro na requisição para realizar a deleção.
 	@DeleteMapping("/deletar/{id}")
 	public ResponseEntity<AluraDto> deletar(@PathVariable Long id) {
 		return aluraService.deletar(id);
