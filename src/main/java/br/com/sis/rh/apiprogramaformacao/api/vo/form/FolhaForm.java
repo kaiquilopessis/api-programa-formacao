6ad53ba0f202656c485ee4b076f4a59b045c3bd0
Guilherme Melo <guilhermeappache@hotmail.com>
2021-11-22 11:49:18 -0300

    Tirei anotações do lombok
    Tirei anotações do lombok

--- src/main/java/br/com/sis/rh/apiprogramaformacao/api/vo/form/FolhaForm.java
+++ src/main/java/br/com/sis/rh/apiprogramaformacao/api/vo/form/FolhaForm.java
@@ -1,20 +1,15 @@
 package br.com.sis.rh.apiprogramaformacao.api.vo.form;
 
 
-import br.com.sis.rh.apiprogramaformacao.api.model.Investimentos;
-import br.com.sis.rh.apiprogramaformacao.api.model.Participante;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-import javax.persistence.Column;
-import javax.persistence.Id;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-@Getter
-@Setter
+import br.com.sis.rh.apiprogramaformacao.api.model.Investimentos;
+import br.com.sis.rh.apiprogramaformacao.api.model.Participante;
+import lombok.NoArgsConstructor;
+
+
 @NoArgsConstructor
 public class FolhaForm {
     private String cpf;
@@ -32,5 +27,54 @@ public class FolhaForm {
         LocalDate dataFormatada = LocalDate.parse(folhaForm.getMesAno(), nome);
         return new Investimentos(participante, dataFormatada, remuneracaoFormatada, encargoFormatado, beneficiosFormatado, folhaForm.getDescricao());
     }
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getMesAno() {
+		return mesAno;
+	}
+
+	public void setMesAno(String mesAno) {
+		this.mesAno = mesAno;
+	}
+
+	public String getRemuneracao() {
+		return remuneracao;
+	}
+
+	public void setRemuneracao(String remuneracao) {
+		this.remuneracao = remuneracao;
+	}
+
+	public String getEncargos() {
+		return encargos;
+	}
+
+	public void setEncargos(String encargos) {
+		this.encargos = encargos;
+	}
+
+	public String getBeneficios() {
+		return beneficios;
+	}
+
+	public void setBeneficios(String beneficios) {
+		this.beneficios = beneficios;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+    
 
 }
--- src/main/java/br/com/sis/rh/apiprogramaformacao/api/vo/form/InvestimentoInstrutorForm.java
+++ src/main/java/br/com/sis/rh/apiprogramaformacao/api/vo/form/InvestimentoInstrutorForm.java
@@ -1,34 +1,61 @@
 package br.com.sis.rh.apiprogramaformacao.api.vo.form;
 
-import br.com.sis.rh.apiprogramaformacao.api.model.Instrutor;
-import br.com.sis.rh.apiprogramaformacao.api.model.Investimentos;
-import br.com.sis.rh.apiprogramaformacao.api.model.Participante;
-import br.com.sis.rh.apiprogramaformacao.api.model.Remuneracao_Instrutor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-@Getter
-@Setter
+import br.com.sis.rh.apiprogramaformacao.api.model.Instrutor;
+import br.com.sis.rh.apiprogramaformacao.api.model.Remuneracao_Instrutor;
+import lombok.NoArgsConstructor;
+
 @NoArgsConstructor
 
 public class InvestimentoInstrutorForm {
 
-    private String cpf;
-    private String mesAno;
-    private String valorHora;
-    private String horasTrabalhadas;
+	private String cpf;
+	private String mesAno;
+	private String valorHora;
+	private String horasTrabalhadas;
 
-    public static Remuneracao_Instrutor converter(InvestimentoInstrutorForm investimentoInstrutorForm, Instrutor instrutor){
-        DateTimeFormatter nome = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        BigDecimal valorHoraFormatado = new BigDecimal(investimentoInstrutorForm.getValorHora());
-        Integer horasTrabalhadasFormatada = Integer.parseInt(investimentoInstrutorForm.getHorasTrabalhadas());
-        LocalDate dataFormatada = LocalDate.parse(investimentoInstrutorForm.getMesAno(), nome);
-        return new Remuneracao_Instrutor(instrutor, dataFormatada, valorHoraFormatado, horasTrabalhadasFormatada);
-    }
-    
+	public static Remuneracao_Instrutor converter(InvestimentoInstrutorForm investimentoInstrutorForm,
+			Instrutor instrutor) {
+		DateTimeFormatter nome = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		BigDecimal valorHoraFormatado = new BigDecimal(investimentoInstrutorForm.getValorHora());
+		Integer horasTrabalhadasFormatada = Integer.parseInt(investimentoInstrutorForm.getHorasTrabalhadas());
+		LocalDate dataFormatada = LocalDate.parse(investimentoInstrutorForm.getMesAno(), nome);
+		return new Remuneracao_Instrutor(instrutor, dataFormatada, valorHoraFormatado, horasTrabalhadasFormatada);
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getMesAno() {
+		return mesAno;
+	}
+
+	public void setMesAno(String mesAno) {
+		this.mesAno = mesAno;
+	}
+
+	public String getValorHora() {
+		return valorHora;
+	}
+
+	public void setValorHora(String valorHora) {
+		this.valorHora = valorHora;
+	}
+
+	public String getHorasTrabalhadas() {
+		return horasTrabalhadas;
+	}
+
+	public void setHorasTrabalhadas(String horasTrabalhadas) {
+		this.horasTrabalhadas = horasTrabalhadas;
+	}
+
 }
