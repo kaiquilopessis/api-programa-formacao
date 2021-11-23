e798aa30002b5b65d8cc9b4555c6684b73f771e3
Guilherme Melo <guilhermeappache@hotmail.com>
2021-11-23 11:19:24 -0300

    Criamos o getter e setter
    Tiramos o lombok

--- src/main/java/br/com/sis/rh/apiprogramaformacao/api/vo/dto/FiltragemFolhaDto.java
+++ src/main/java/br/com/sis/rh/apiprogramaformacao/api/vo/dto/FiltragemFolhaDto.java
@@ -5,42 +5,173 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+
+import br.com.sis.rh.apiprogramaformacao.api.controller.FolhaController;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-@Getter
-@Setter
 @Entity
 @NoArgsConstructor
 public class FiltragemFolhaDto {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "cpf_participante")
-    private String cpfParticipante;
-    @Column(name = "nome_participante")
-    private String nomeParticipante;
-    @Column(name = "nome_programa")
-    private String nomeFormacao;
-    @Column(name = "nome_turma")
-    private String nomeTurma;
-    @Column(name = "bolsa_aux")
-    private BigDecimal bolsaAux;
-    @Column(name = "beneficios")
-    private BigDecimal beneficios;
-    @Column(name = "convenio")
-    private BigDecimal convenio;
-    @Column(name = "hr_extra")
-    private BigDecimal horaExtra;
-    @Column(name = "beneficio_legislacao")
-    private BigDecimal beneficioLegislacao;
-    @Column(name = "remun_esporadica")
-    private BigDecimal remuneracaoEsporadica;
-    @Column(name = "remun_extra")
-    private BigDecimal remuneracaoExtra;
-    @Column(name = "alura")
-    private BigDecimal alura;
-    @Column(name = "data_fim_programa")
-    private LocalDate dataFim;
-    @Column(name = "data_inicio_programa")
-    private LocalDate dataInicio;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "cpf_participante")
+	private String cpfParticipante;
+
+	@Column(name = "nome_participante")
+	private String nomeParticipante;
+
+	@Column(name = "nome_programa")
+	private String nomeFormacao;
+
+	@Column(name = "nome_turma")
+	private String nomeTurma;
+
+	@Column(name = "bolsa_aux")
+	private BigDecimal bolsaAux;
+
+	@Column(name = "beneficios")
+	private BigDecimal beneficios;
+
+	@Column(name = "convenio")
+	private BigDecimal convenio;
+
+	@Column(name = "hr_extra")
+	private BigDecimal horaExtra;
+
+	@Column(name = "beneficio_legislacao")
+	private BigDecimal beneficioLegislacao;
+
+	@Column(name = "remun_esporadica")
+	private BigDecimal remuneracaoEsporadica;
+
+	@Column(name = "remun_extra")
+	private BigDecimal remuneracaoExtra;
+
+	@Column(name = "alura")
+	private BigDecimal alura;
+
+	@Column(name = "data_fim_programa")
+	private LocalDate dataFim;
+
+	@Column(name = "data_inicio_programa")
+	private LocalDate dataInicio;
+
+	public FiltragemFolhaDto() {
+
+	}
+
+	public String getCpfParticipante() {
+		return cpfParticipante;
+	}
+
+	public void setCpfParticipante(String cpfParticipante) {
+		this.cpfParticipante = cpfParticipante;
+	}
+
+	public String getNomeParticipante() {
+		return nomeParticipante;
+	}
+
+	public void setNomeParticipante(String nomeParticipante) {
+		this.nomeParticipante = nomeParticipante;
+	}
+
+	public String getNomeFormacao() {
+		return nomeFormacao;
+	}
+
+	public void setNomeFormacao(String nomeFormacao) {
+		this.nomeFormacao = nomeFormacao;
+	}
+
+	public String getNomeTurma() {
+		return nomeTurma;
+	}
+
+	public void setNomeTurma(String nomeTurma) {
+		this.nomeTurma = nomeTurma;
+	}
+
+	public BigDecimal getBolsaAux() {
+		return bolsaAux;
+	}
+
+	public void setBolsaAux(BigDecimal bolsaAux) {
+		this.bolsaAux = bolsaAux;
+	}
+
+	public BigDecimal getBeneficios() {
+		return beneficios;
+	}
+
+	public void setBeneficios(BigDecimal beneficios) {
+		this.beneficios = beneficios;
+	}
+
+	public BigDecimal getConvenio() {
+		return convenio;
+	}
+
+	public void setConvenio(BigDecimal convenio) {
+		this.convenio = convenio;
+	}
+
+	public BigDecimal getHoraExtra() {
+		return horaExtra;
+	}
+
+	public void setHoraExtra(BigDecimal horaExtra) {
+		this.horaExtra = horaExtra;
+	}
+
+	public BigDecimal getBeneficioLegislacao() {
+		return beneficioLegislacao;
+	}
+
+	public void setBeneficioLegislacao(BigDecimal beneficioLegislacao) {
+		this.beneficioLegislacao = beneficioLegislacao;
+	}
+
+	public BigDecimal getRemuneracaoEsporadica() {
+		return remuneracaoEsporadica;
+	}
+
+	public void setRemuneracaoEsporadica(BigDecimal remuneracaoEsporadica) {
+		this.remuneracaoEsporadica = remuneracaoEsporadica;
+	}
+
+	public BigDecimal getRemuneracaoExtra() {
+		return remuneracaoExtra;
+	}
+
+	public void setRemuneracaoExtra(BigDecimal remuneracaoExtra) {
+		this.remuneracaoExtra = remuneracaoExtra;
+	}
+
+	public BigDecimal getAlura() {
+		return alura;
+	}
+
+	public void setAlura(BigDecimal alura) {
+		this.alura = alura;
+	}
+
+	public LocalDate getDataFim() {
+		return dataFim;
+	}
+
+	public void setDataFim(LocalDate dataFim) {
+		this.dataFim = dataFim;
+	}
+
+	public LocalDate getDataInicio() {
+		return dataInicio;
+	}
+
+	public void setDataInicio(LocalDate dataInicio) {
+		this.dataInicio = dataInicio;
+	}
+
 }
--- src/main/java/br/com/sis/rh/apiprogramaformacao/api/vo/dto/FiltragemInstrutorDto.java
+++ src/main/java/br/com/sis/rh/apiprogramaformacao/api/vo/dto/FiltragemInstrutorDto.java
@@ -1,23 +1,18 @@
 package br.com.sis.rh.apiprogramaformacao.api.vo.dto;
 
-import org.apache.tomcat.jni.Local;
-
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-
 public class FiltragemInstrutorDto {
-    private String cpfInstrutor;
-    private String nomeInstrutor;
-    private String nomePrograma;
-    private String nomeTurma;
-    private Integer qtdHora;
-    private BigDecimal vlrHora;
+	private String cpfInstrutor;
+	private String nomeInstrutor;
+	private String nomePrograma;
+	private String nomeTurma;
+	private Integer qtdHora;
+	private BigDecimal vlrHora;
 	private LocalDate dataLancamento;
-    private LocalDate dataFim;
-    
-    
-    
+	private LocalDate dataFim;
+
 	public FiltragemInstrutorDto(String cpfInstrutor, String nomeInstrutor, String nomePrograma, String nomeTurma,
 			Integer qtdHora, BigDecimal vlrHora, LocalDate dataLancamento, LocalDate dataFim) {
 		this.cpfInstrutor = cpfInstrutor;
@@ -29,53 +24,73 @@ public class FiltragemInstrutorDto {
 		this.dataLancamento = dataLancamento;
 		this.dataFim = dataFim;
 	}
-	
-	public FiltragemInstrutorDto () {
-		
+
+	public FiltragemInstrutorDto() {
+
 	}
-	
+
 	public String getCpfInstrutor() {
 		return cpfInstrutor;
 	}
+
 	public void setCpfInstrutor(String cpfInstrutor) {
 		this.cpfInstrutor = cpfInstrutor;
 	}
+
 	public String getNomeInstrutor() {
 		return nomeInstrutor;
 	}
+
 	public void setNomeInstrutor(String nomeInstrutor) {
 		this.nomeInstrutor = nomeInstrutor;
 	}
+
 	public String getNomePrograma() {
 		return nomePrograma;
 	}
+
 	public void setNomePrograma(String nomePrograma) {
 		this.nomePrograma = nomePrograma;
 	}
+
 	public String getNomeTurma() {
 		return nomeTurma;
 	}
+
 	public void setNomeTurma(String nomeTurma) {
 		this.nomeTurma = nomeTurma;
 	}
+
 	public Integer getQtdHora() {
 		return qtdHora;
 	}
+
 	public void setQtdHora(Integer qtdHora) {
 		this.qtdHora = qtdHora;
 	}
+
 	public BigDecimal getVlrHora() {
 		return vlrHora;
 	}
+
 	public void setVlrHora(BigDecimal vlrHora) {
 		this.vlrHora = vlrHora;
 	}
+
 	public LocalDate getDataFim() {
 		return dataFim;
 	}
+
 	public void setDataFim(LocalDate dataFim) {
 		this.dataFim = dataFim;
 	}
-    
-    
+
+	public LocalDate getDataLancamento() {
+		return dataLancamento;
+	}
+
+	public void setDataLancamento(LocalDate dataLancamento) {
+		this.dataLancamento = dataLancamento;
+	}
+
 }
