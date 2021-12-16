d3ae965a62d133dfdf741282f6d43d0fb708a71f
felipesalmazo82 <felipesalmazo@sisconsultoria.com.br>
2021-12-16 09:23:29 -0300

    Retirando o lombok

--- src/main/java/br/com/sis/rh/apiprogramaformacao/api/vo/dto/input/LoginInput.java
+++ src/main/java/br/com/sis/rh/apiprogramaformacao/api/vo/dto/input/LoginInput.java
@@ -2,19 +2,34 @@ package br.com.sis.rh.apiprogramaformacao.api.vo.dto.input;
 
 import javax.validation.constraints.NotBlank;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class LoginInput {
-	
+
 	@NotBlank
 	private String matricula;
-	
+
 	@NotBlank
 	private String senha;
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public LoginInput(@NotBlank String matricula, @NotBlank String senha) {
+		super();
+		this.matricula = matricula;
+		this.senha = senha;
+	}
 
 }
--- src/main/java/br/com/sis/rh/apiprogramaformacao/core/ad/UsuarioAD.java
+++ src/main/java/br/com/sis/rh/apiprogramaformacao/core/ad/UsuarioAD.java
@@ -5,22 +5,18 @@ import java.io.Serializable;
 import javax.naming.directory.Attribute;
 import javax.naming.directory.Attributes;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.SneakyThrows;
 
-@Data
-@NoArgsConstructor
-public class UsuarioAD implements Serializable{
-	
+public class UsuarioAD implements Serializable {
+
 	private static final long serialVersionUID = 3773551911483960017L;
-	
+
 	private String nome;
 	private String sobrenome;
 	private String matricula;
 	private String email;
 	private String telefone;
-	
+
 	@SneakyThrows
 	public UsuarioAD(Attributes attributes) {
 		nome = getAttribute(attributes.get("givenname"));
@@ -29,13 +25,57 @@ public class UsuarioAD implements Serializable{
 		email = getAttribute(attributes.get("mail"));
 		telefone = getAttribute(attributes.get("telephonenumber"));
 	}
-	
+
 	private String getAttribute(Attribute attribute) {
 		try {
 			return attribute != null ? attribute.get().toString() : null;
 		} catch (Exception e) {
 			return null;
 		}
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public UsuarioAD() {
+
 	}
 
 }
