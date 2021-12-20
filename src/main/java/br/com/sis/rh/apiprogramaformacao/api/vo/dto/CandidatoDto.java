a6e2b8c5980e8430e5a0df352533ccbfa2a967e6
felipesalmazo82 <felipesalmazo@sisconsultoria.com.br>
2021-12-20 09:35:02 -0300

    alteracao DTO candidato

--- src/main/java/br/com/sis/rh/apiprogramaformacao/api/vo/dto/CandidatoDto.java
+++ src/main/java/br/com/sis/rh/apiprogramaformacao/api/vo/dto/CandidatoDto.java
@@ -3,122 +3,198 @@ package br.com.sis.rh.apiprogramaformacao.api.vo.dto;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Locale;
 import java.util.stream.Collectors;
 
 import br.com.sis.rh.apiprogramaformacao.api.model.Candidato;
 
 public class CandidatoDto {
-    private Long id;
-    private String nome;
-    private String telefone;
-    private String fonteRecrutamento;
-    private LocalDate dataAgendamento;
-    private String status;
-    private BigDecimal testeLogico;
-    private String notaDisc;
-    private String observacao;
-    private String processoSeletivo;
-    private Long processoSeletivoId;
+	private Long id;
+	private String nome;
+	private String telefone;
+	private String fonteRecrutamento;
+	private LocalDate dataAgendamento;
+	private String status;
+	private BigDecimal testeLogico;
+	private String notaDisc;
+	private String observacao;
+	private String processoSeletivo;
+	private Long processoSeletivoId;
+	private String email;
+	private String semestre;
+	private String periodoCurso;
+	private LocalDate dataConclusao;
+	private String duracaoCurso;
+	private String endereco;
+	private String IndicacaoVaga;
 
-    public CandidatoDto(Candidato candidato){
-        this.id = candidato.getId();
-        this.nome = candidato.getNome();
-        this.fonteRecrutamento = candidato.getFonteRecrutamento();
-        this.telefone = candidato.getTelefone();
-        this.dataAgendamento = candidato.getDataAgendamento();
-        this.status = candidato.getStatus().replace("_", " ");
-        this.testeLogico = candidato.getTesteLogico();
-        this.observacao = candidato.getObservacao();
-        this.processoSeletivo = candidato.getProcessoSeletivo().getNome();
-        this.processoSeletivoId = candidato.getProcessoSeletivo().getId();
+	public CandidatoDto(Candidato candidato) {
+		this.id = candidato.getId();
+		this.nome = candidato.getNome();
+		this.fonteRecrutamento = candidato.getFonteRecrutamento();
+		this.telefone = candidato.getTelefone();
+		this.dataAgendamento = candidato.getDataAgendamento();
+		this.status = candidato.getStatus().replace("_", " ");
+		this.testeLogico = candidato.getTesteLogico();
+		this.observacao = candidato.getObservacao();
+		this.processoSeletivo = candidato.getProcessoSeletivo().getNome();
+		this.processoSeletivoId = candidato.getProcessoSeletivo().getId();
+		this.email = candidato.getEmail();
+		this.semestre = candidato.getSemestreFaculdade();
+		this.periodoCurso = candidato.getPeriodoCurso();
+		this.dataConclusao = candidato.getDataConclusao();
+		this.duracaoCurso = candidato.getDuracaoCurso();
+		this.email = candidato.getEndereco();
+		this.IndicacaoVaga = candidato.getIndicacaoVaga();
+	}
 
-    }
+	public CandidatoDto() {
+	}
 
-    public CandidatoDto() {
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public String getNome() {
+		return nome;
+	}
 
-    public String getNome() {
-        return nome;
-    }
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+	public String getTelefone() {
+		return telefone;
+	}
 
-    public String getTelefone() {
-        return telefone;
-    }
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
 
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
+	public String getFonteRecrutamento() {
+		return fonteRecrutamento;
+	}
 
-    public String getFonteRecrutamento() {
-        return fonteRecrutamento;
-    }
+	public void setFonteRecrutamento(String fonteRecrutamento) {
+		this.fonteRecrutamento = fonteRecrutamento;
+	}
 
-    public void setFonteRecrutamento(String fonteRecrutamento) {
-        this.fonteRecrutamento = fonteRecrutamento;
-    }
+	public LocalDate getDataAgendamento() {
+		return dataAgendamento;
+	}
 
-    public LocalDate getDataAgendamento() {
-        return dataAgendamento;
-    }
+	public void setDataAgendamento(LocalDate dataAgendamento) {
+		this.dataAgendamento = dataAgendamento;
+	}
 
-    public void setDataAgendamento(LocalDate dataAgendamento) {
-        this.dataAgendamento = dataAgendamento;
-    }
+	public String getStatus() {
+		return status;
+	}
 
-    public String getStatus() {
-        return status;
-    }
+	public void setStatus(String status) {
+		this.status = status;
+	}
 
-    public void setStatus(String status) {
-        this.status = status;
-    }
+	public BigDecimal getTesteLogico() {
+		return testeLogico;
+	}
 
-    public BigDecimal getTesteLogico() {
-        return testeLogico;
-    }
+	public void setTesteLogico(BigDecimal testeLogico) {
+		this.testeLogico = testeLogico;
+	}
 
-    public void setTesteLogico(BigDecimal testeLogico) {
-        this.testeLogico = testeLogico;
-    }
+	public String getObservacao() {
+		return observacao;
+	}
 
-    public String getObservacao() {
-        return observacao;
-    }
+	public void setObservacao(String observacao) {
+		this.observacao = observacao;
+	}
 
-    public void setObservacao(String observacao) {
-        this.observacao = observacao;
-    }
+	public String getProcessoSeletivo() {
+		return processoSeletivo;
+	}
 
-    public String getProcessoSeletivo() {
-        return processoSeletivo;
-    }
+	public void setProcessoSeletivo(String processoSeletivo) {
+		this.processoSeletivo = processoSeletivo;
+	}
 
-    public void setProcessoSeletivo(String processoSeletivo) {
-        this.processoSeletivo = processoSeletivo;
-    }
+	public Long getProcessoSeletivoId() {
+		return processoSeletivoId;
+	}
 
-    public Long getProcessoSeletivoId() {
-        return processoSeletivoId;
-    }
+	public void setProcessoSeletivoId(Long processoSeletivoId) {
+		this.processoSeletivoId = processoSeletivoId;
+	}
 
-    public void setProcessoSeletivoId(Long processoSeletivoId) {
-        this.processoSeletivoId = processoSeletivoId;
-    }
+	public String getNotaDisc() {
+		return notaDisc;
+	}
 
-    public static List<CandidatoDto> converter(List<Candidato> candidato){
+	public void setNotaDisc(String notaDisc) {
+		this.notaDisc = notaDisc;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSemestre() {
+		return semestre;
+	}
+
+	public void setSemestre(String semestre) {
+		this.semestre = semestre;
+	}
+
+	public String getPeriodoCurso() {
+		return periodoCurso;
+	}
+
+	public void setPeriodoCurso(String periodoCurso) {
+		this.periodoCurso = periodoCurso;
+	}
+
+	public LocalDate getDataConclusao() {
+		return dataConclusao;
+	}
+
+	public void setDataConclusao(LocalDate dataConclusao) {
+		this.dataConclusao = dataConclusao;
+	}
+
+	public String getDuracaoCurso() {
+		return duracaoCurso;
+	}
+
+	public void setDuracaoCurso(String duracaoCurso) {
+		this.duracaoCurso = duracaoCurso;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getIndicacaoVaga() {
+		return IndicacaoVaga;
+	}
+
+	public void setIndicacaoVaga(String indicacaoVaga) {
+		IndicacaoVaga = indicacaoVaga;
+	}
+
+	public static List<CandidatoDto> converter(List<Candidato> candidato) {
 		return candidato.stream().map(CandidatoDto::new).collect(Collectors.toList());
 	}
 
