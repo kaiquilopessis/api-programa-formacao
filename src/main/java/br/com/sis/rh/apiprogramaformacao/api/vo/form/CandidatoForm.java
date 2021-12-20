1682ab20350fd124f06f1f2185f2e22a7f509724
GustavoJulianosis <gustavojuliano@sisconsultoria.com.br>
2021-12-20 16:05:46 -0300

    Ajustes no formulário de envio Candidato

--- src/main/java/br/com/sis/rh/apiprogramaformacao/api/vo/form/CandidatoForm.java
+++ src/main/java/br/com/sis/rh/apiprogramaformacao/api/vo/form/CandidatoForm.java
@@ -38,12 +38,13 @@ public class CandidatoForm {
         ProcessoSeletivo processoSeletivo = repository.getById(idProcessoSeletivo);
 
         LocalDate data = LocalDate.parse(this.dataAgendamento, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-        if (this.semestreFaculdade.equals(null)) {
+        if (this.dataConclusao.equals("")) {
         	
 			return new Candidato(nome, telefone, data, testeLogico, status, observacao, curriculo.getBytes(), disc.getBytes(), fonteRecrutamento, processoSeletivo, email, endereco, indicacaoVaga);
-		}
-        LocalDate dataConclusao = LocalDate.parse(this.dataConclusao, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-        return new Candidato(nome, telefone, data, testeLogico, status, observacao, curriculo.getBytes(), disc.getBytes(), fonteRecrutamento, processoSeletivo, email, semestreFaculdade, periodoCurso, dataConclusao, duracaoCurso, endereco, indicacaoVaga);
+		} else{
+            LocalDate dataConclusao = LocalDate.parse(this.dataConclusao, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            return new Candidato(nome, telefone, data, testeLogico, status, observacao, curriculo.getBytes(), disc.getBytes(), fonteRecrutamento, processoSeletivo, email, semestreFaculdade, periodoCurso, dataConclusao, duracaoCurso, endereco, indicacaoVaga);
+        }
     }
 
     public String getNome() {
