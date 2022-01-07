7d71e6c6cd9981957bf94f38cace32920fccb49b
felipesalmazo82 <felipesalmazo@sisconsultoria.com.br>
2022-01-07 14:02:31 -0300

    alteracao dos links

--- src/main/java/br/com/sis/rh/apiprogramaformacao/api/config/CorsConfig.java
+++ src/main/java/br/com/sis/rh/apiprogramaformacao/api/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://192.168.30.162:8080")
+                .allowedOrigins("http://localhost:8080")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
