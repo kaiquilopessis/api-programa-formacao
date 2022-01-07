716c3307ddc71b766162dd4e1e9ffbfb2a8a5053
felipesalmazo82 <felipesalmazo@sisconsultoria.com.br>
2022-01-07 14:22:39 -0300

    implementação do swagger

---
+++ src/main/java/br/com/sis/rh/apiprogramaformacao/core/swagger/SwaggerConfiguration.java
@@ -0,0 +1,27 @@
+package br.com.sis.rh.apiprogramaformacao.core.swagger;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+
+import br.com.sis.rh.apiprogramaformacao.api.model.LoginAD;
+import br.com.sis.rh.apiprogramaformacao.core.ad.UsuarioAD;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+public class SwaggerConfiguration {
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("br.com.sis.rh.apiprogramaformacao")).paths(PathSelectors.ant("/**")).build()
+				.ignoredParameterTypes(LoginAD.class, UsuarioAD.class).globalOperationParameters(
+						Arrays.asList(new ParameterBuilder().name("Authorization").description("Header para Token JWT")
+								.modelRef(new ModelRef("string")).parameterType("header").required(false).build()));
+	}
+
+}
