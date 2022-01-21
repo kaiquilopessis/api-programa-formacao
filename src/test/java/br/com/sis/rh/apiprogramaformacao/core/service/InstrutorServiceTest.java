aac782e63c0df6276b416b2aa1b9c25c886adde1
GustavoJulianosis <gustavojuliano@sisconsultoria.com.br>
2022-01-21 14:24:59 -0300

    Anotação para Mockar Usuário

---
+++ src/test/java/br/com/sis/rh/apiprogramaformacao/core/service/InstrutorServiceTest.java
@@ -0,0 +1,13 @@
+package br.com.sis.rh.apiprogramaformacao.core.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class InstrutorServiceTest {
+
+    @Autowired
+    private InstrutorService instrutorService;
+
+
+}
--- src/test/java/br/com/sis/rh/apiprogramaformacao/core/service/MatriculaServiceTest.java
+++ src/test/java/br/com/sis/rh/apiprogramaformacao/core/service/MatriculaServiceTest.java
@@ -1,20 +1,16 @@
 package br.com.sis.rh.apiprogramaformacao.core.service;
 
-import br.com.sis.rh.apiprogramaformacao.api.controller.ADController;
 import br.com.sis.rh.apiprogramaformacao.api.model.LoginAD;
-import br.com.sis.rh.apiprogramaformacao.api.model.Perfil;
 import br.com.sis.rh.apiprogramaformacao.api.vo.dto.LoginADDto;
 import br.com.sis.rh.apiprogramaformacao.api.vo.dto.input.LoginInput;
 import br.com.sis.rh.apiprogramaformacao.api.vo.form.LoginADForm;
 import br.com.sis.rh.apiprogramaformacao.core.ad.ConnectAD;
-import br.com.sis.rh.apiprogramaformacao.core.ad.UsuarioAD;
-import br.com.sis.rh.apiprogramaformacao.core.repository.PerfilRepository;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.test.context.support.WithMockUser;
 
 import javax.naming.NamingException;
 
@@ -29,28 +25,36 @@ public class MatriculaServiceTest {
     @Autowired
     private MatriculaService matriculaService;
 
+    private static LoginInput loginInput = new LoginInput("blisboa", "sis@123");
+
+    private static ConnectAD connectAD = new ConnectAD();
+
     private static LoginADForm loginADForm = new LoginADForm();
 
     @BeforeAll
-    public void executaAntesDeTodosOsMetodos () throws NamingException {
+    public static void executaAntesDeTodosOsMetodos () throws NamingException {
         loginADForm.setMatricula("teste");
         loginADForm.setPerfil("ROLE_ADMINISTRADOR");
+
+//        connectAD.getUser(loginInput.getMatricula(), loginInput.getSenha());
     }
 
     @Test
-    public void deveriaRetornarUmaListaComTodasAsMatriculas () {
+    public void deveriaRetornarUmaListaComTodasAsMatriculas (){
         List<LoginADDto> lista = matriculaService.buscarTodos();
 
         assertNotNull(lista);
     }
 
     @Test
+    @WithMockUser("testeUnitarioJUnit")
     public void deveriaCriarUmaMatricula () {
         LoginAD matricula = matriculaService.criaMatricula(loginADForm);
         assertSame(LoginAD.class, matricula.getClass());
     }
 
     @Test
+    @WithMockUser("testeUnitarioJUnit")
     public void deveriaExcluirUmaMatricula () {
         assertEquals(ResponseEntity.ok().build(), matriculaService.deletaMatricula("teste"));
     }
