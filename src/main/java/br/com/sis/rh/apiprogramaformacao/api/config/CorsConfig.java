125c7779c8208001542e32c1fc75a9db511d6aab
GustavoJulianosis <gustavojuliano@sisconsultoria.com.br>
2022-02-01 16:25:19 -0300

    Adicionado try catch no ConnectAD

--- src/main/java/br/com/sis/rh/apiprogramaformacao/api/config/CorsConfig.java
+++ src/main/java/br/com/sis/rh/apiprogramaformacao/api/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080")
+                .allowedOrigins("http://192.168.38.10:8080")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
